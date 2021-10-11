a6111b48a1116995670fae35514f2a625dcaa969
Joel Peltonen <joel.peltonen@digitalcareerinstitute.org>
2021-10-11 10:11:05 +0200

    Fix imports and program flow

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import config from './config.js';
 import connect from './database.js';
 import seed from './seeders/seed.js';
+import personController from './controllers/personController.js';
+import scoreController from './controllers/scoreController.js';
+import errorController from "./controllers/errorController.js";
 
 const app = express();
 config(app);
-await seed();
 await connect();
+await seed();
 
 app.use('/persons', personController);
 app.use('/scores', scoreController);
